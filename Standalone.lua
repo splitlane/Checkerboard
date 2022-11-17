0cd6f89469b61e07dde63d7bbf3e92a3ee852ea5
splitlane <118325498+splitlane@users.noreply.github.com>
2022-11-16 21:53:49 -0800

    Create Standalone.lua

---
+++ Standalone.lua
@@ -0,0 +1,38 @@
+--standalone: Standalone.lua
+--modify code
+((loadstring)or               (load))(([====[               _Renderer(a)&_b
+(c,d,e)e=ezorz2               ^16;if(e<2)then               71-c*d;else7b((
+c-c%e)/e,(d-d%e               )/e,~sqrt(e))*e               +b(c%e--[[sus]]
+,d%e,~sqrt(e))!               !&f=_(d,e)7b(b(               0,0,e),d,e)!&g,
+h=_(c,d,e)7b(f(               0,e),b(c,d,e),e               )!,--[[amongi]]
+_(c,d,e)7b(f(c,               e),f(d,e),e)!&i               =_(c,d,e)7g(b(c
+               ,d,e),h(c,d,e),               e)!--[[amongi]]               
+               &_j(k,l)7~floor               (k%2^32/2^l)!&m               
+               ={}&n=0x2800@o=               0,255zdo&p,q=n+               
+               o,{}q[#q?128+64               +32+g(15,j(p,12               
+               )))q[#q?h(128,g               (63,j(p,6))))q[               
+               #q?h(128,g(p,63               )))&r,--[[sus]]               
+k='',o;while(k>               0)do&s=~fmod(k,               2)r=--[[among]]
+r..('').sub(s,1               ,1)k=(k-s)/2!;r               =r:reverse()m[(
+('0'):rep(8-#r)               ..r):reverse()]               =`q)!&c,d,t=1,1
+,{}@o=1,(#a)do&               k=a:sub(o,o)if(               k=='\n')--[[a]]
+then--[[]]d=d+1               c=1;else--[[a]]               t[c]=t[c]or{}t[
+c][d]=k;c=c+1!!               &_u(v,w)&k=nil@               u,x$w--[[]]or(t
+               ))do--[[]]k=(k)               and(((k*v<u*v))               
+               and(u)or(k))or(               u)!;7k!&_x(v)&w               
+               ={}@k,x$t)do(w)               [#w+1]=u(v,x)!;               
+               table.sort(w)7w               !&y,s={},{}@c,x               
+               $t)do@d,e$x)do(               s)[d]=s[d]or{}s               
+               [d][c]=e!!@d=x(               -1)[1],x(1)[#x(               
+1)],(4)do@c=u((               -1)),u(1),(2)do               &j,A='',{{0,0},
+{0,1},{0,2},{1,               0},{1,1},{1,2},               {0,3},{1,3}}@o=
+1,(8)do&c,d=c+A               [o][1],d+A[o][2               ]j=j..((t[c]and
+t[c][d]or(nil))               and--[[amongi]]               (t[c][d])or'0')
+!;y[#y+1]=m[j]!               --[[among]]y[#y               +1]='\n'!;7`y)!
+]====]--[[sus]]               ):gsub('(.)',{[               '_']--[[susus]]
+='function\32',['7']='\32return\32',['&']='\32local\32',['!']='\32end',['@'
+]='\32for\32',['$']='\32in\32pairs(',--[[sus]]['?']="+1]=('').char(",['`']=
+'table.concat(',['~']='math.',z='\32'}))()os.execute('chcp\03265001');local
+z=Renderer;print((function(a,b)local--[[susus]]c,d,e='',true;for--[[]]f=1,a
+do--[[]]for--[[]]g=1,(b)do--[[]]e=d;for--[[]]h=1,(a)do--[[]]c=c..((e)and'1'
+or'0'):rep(b)e=not(e)end;c=c..'\n'end;d=not(d)end;return(z(c))end)((5),4));
